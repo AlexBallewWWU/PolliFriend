87320f21495f9e75b5a57281b6f3f9677ad1be2a
Alex Ballew <Ajb8241@gmail.com>
2025-05-26 16:02:10 -0700

    functionality done for plant pop up and added more plants

--- PolliFriend/frontend/src/components/PlantSelection/PlantSelection.jsx
+++ PolliFriend/frontend/src/components/PlantSelection/PlantSelection.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './PlantSelection.css'
+import temp from '../../assets/Osoberry-Image.jpeg'
 
 function PlantSelection({plantData}) {
 
@@ -10,6 +11,8 @@ function PlantSelection({plantData}) {
       <Category name={"Shrub"} plantData={plantData}></Category>
       <Category name={"Perennial"} plantData={plantData}></Category>
       <Category name={"Tree"} plantData={plantData}></Category>
+      <Category name={"Ferns"} plantData={plantData}></Category>
+      <Category name={"Fruits"} plantData={plantData}></Category>
     </div>
     )
 }
@@ -42,7 +45,18 @@ function Plants({name, plantData}) {
       {console.log(item.type + " " + name)}
 
       return (
-        item.type == name && <div className='plant-container'>{item.name}</div>
+        item.type == name && 
+        <div className='plant-container'>
+          <img src={item.image} alt='img' style={{width: '20%', height: '100%'}}></img>
+          <div className='plant-info-container'>
+            <div className='plant-title-container'>
+              {item.name} 
+              <div className='plus-button'> + </div>
+            </div>
+            <p> {item.info}</p>
+            <strong> Scores: Carbon: ({item.carbonScore}/3), Pollination: ({item.pollinationScore}/10), Water: ({item.waterScore}/6) </strong>
+          </div>
+        </div>
       ) 
     })
   )
