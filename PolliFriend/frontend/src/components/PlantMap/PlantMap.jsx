14bd490dcb9ad4db050e495fcd248d257acf332a
Bryce Richmond <bryce.richmond@gmail.com>
2025-05-26 21:49:25 -0700

    Cleaned up code

--- PolliFriend/frontend/src/components/PlantMap/PlantMap.jsx
+++ PolliFriend/frontend/src/components/PlantMap/PlantMap.jsx
@@ -3,26 +3,25 @@ import './PlantMap.css'
 import map from '../../assets/istockphoto-1322626419-612x612.jpg'
 function PlantMap() {
 
-
+    {/* Need to condionally call this function based on the click*/}
     const handleMapClick = (e) => {
-        // useEffect(() => {
-            const rect = e.target.getBoundingClientRect();
-        // }, [handleMapClick]);
-        
+        // need this to refresh as the user scrolls down   
+        const rect = e.target.getBoundingClientRect();
         const x = e.clientX;
         const y = e.clientY;
 
         setItems([...items, { x, y, type: "plant" }]);
     };
+    //Temporary tests, to connect to the database
     const [items, setItems] = useState([
         { id: 1, type: 'plant', label: 'Tomato' },
         { id: 2, type: 'building', label: 'Shed'},
     ]);
-    // 
     
     return (
         <>
             <h1 style={{ color: "black" }}>
+                {/* Initial Idea for grid, mostl likely will scrap but kept this here in case */}
                 {/* <form >
                     <label>
                         Yard Dimensions:
@@ -34,6 +33,7 @@ function PlantMap() {
             </h1>
             <div className="map-container" onClick={handleMapClick} >
                 <img src={map} alt="Garden Map" style={{ width: '100%', height: '100%', objectFit: 'cover' }}/>
+                {/* display clicks, could also be the issue with the dots */}
                 {items.map((item, idx) => (
                     <div
                         key={idx}
