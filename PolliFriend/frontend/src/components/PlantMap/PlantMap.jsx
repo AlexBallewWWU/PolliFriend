bebd8348fa4877bc535fc2867ace1a839f28527a
Alex Ballew <Ajb8241@gmail.com>
2025-06-11 12:49:39 -0700

    plan page done

--- PolliFriend/frontend/src/components/PlantMap/PlantMap.jsx
+++ PolliFriend/frontend/src/components/PlantMap/PlantMap.jsx
@@ -1,14 +1,12 @@
 import { useEffect, useState } from 'react'
 import './PlantMap.css'
-import map from '../../assets/istockphoto-1322626419-612x612.jpg'
-import shrub from '../../assets/plant.svg'
-import grid from '../../assets/grid.png'
-var i = 1;
+import map from '../../assets/image.png'
+
 function PlantMap({ curPlantIcon, curPlantInfo, setCarbonScore, setWaterScore, setPollinScore}) {
-    // console.log(curPlantInfo.plantData.plants[i].type)
-    // useEffect = ({
-    //     handleMapClick
-    // }, [])
+
+    const [items, setItems] = useState([]);
+
+
     const handleMapClick = (e) => {
         // need this to refresh as the user scrolls down
         const rect = e.target.getBoundingClientRect();
@@ -17,59 +15,73 @@ function PlantMap({ curPlantIcon, curPlantInfo, setCarbonScore, setWaterScore, s
         const y = e.screenY - rect.top;
         console.log(curPlantInfo)
 
-        setItems([...items, { x, y, type: "plant" }]);
-        // Variables are set up I just need to add calcs here
-        console.log(curPlantInfo)
-        setCarbonScore();
-        setPollinScore();
-        setWaterScore();
-        // setItems((prevItems) =>
-        //     prevItems.map((item, i) => (i === index ? newValue : item))
-        // );
-        i++;
+        setItems([...items, { x, y, plantInfo: curPlantInfo, url: curPlantIcon }]);
+
+        const stats = calculateStats(curPlantInfo);
+        console.log(stats)
+
+        setCarbonScore(stats.carbonScore);
+        setPollinScore(stats.pollinScore);
+        setWaterScore(stats.waterScore);
     };
-    //Temporary tests, to connect to the database
-    const [items, setItems] = useState([]);
-    // document.addEventListener("scroll", handleMapClick);
+
+    // function to calculate stats for graph
+    const calculateStats = (curPlantInfo) => {
+
+        let carbon = 0;
+        let pollin = 0;
+        let water = 0;
+        let total = 0;
+
+            // state is aysnc so manual update
+        if (curPlantInfo.waterScore) {
+            carbon = curPlantInfo.carbonScore/3;
+            pollin = curPlantInfo.pollinationScore/10;
+            water = curPlantInfo.waterScore/6;
+            total = 1;
+        }
+
+            // all prev items
+        items.map( item => {
+            console.log(item.plantInfo.pollinationScore)
+            if (item.plantInfo.waterScore) {
+                carbon += item.plantInfo.carbonScore/3;
+                pollin += item.plantInfo.pollinationScore/10;
+                water += item.plantInfo.waterScore/6;
+                total += 1;
+            }
+        })
+        
+        return {carbonScore : (carbon/total) * 100, pollinScore : (pollin/total) * 100, waterScore : (water/total) * 100}
+    }
 
     return (
         <>
-            <h1 style={{ color: "black" }}>
-                {/* Initial Idea for grid, mostl likely will scrap but kept this here in case */}
-                {/* <form >
-                    <label>
-                        Yard Dimensions:
-                        <input type="number" value={x} onChange={(e) => setX(e.target.value)} />
-                        <input type="number" value={y} onChange={(e) => setY(e.target.value)} />
-                    </label>
-                </form> */}
-
-            </h1>
+            <h1 style={{ color: "black" }}> </h1>
             <div className="map-container" onClick={handleMapClick} >
-                <img src={grid} alt="Garden Map" style={{ width: '100%', height: '100%', objectFit: 'cover', opacity: '30%' }} />
+                <img src={map} alt="Garden Map" style={{ width: '100%', height: '100%', objectFit: 'cover'}} />
 
-                {/* display clicks, could also be the issue with the dots */}
                 {items.map((item, idx) => (
                     <div
                         key={idx}
-                        className={`item ${item.type}`}
+                        className={`item.plant`}
                         style={{
                             position: 'absolute',
                             top: item.y,
                             left: item.x,
-                            backgroundImage: `url(${curPlantIcon})`,
+                            backgroundImage: `url(${item.url})`,
                             width: '100px',
                             height: '100px',
                             borderRadius: '50%',
                             backgroundPosition: 'center',
                             backgroundSize: '100%',
                             transform: 'translate(-50%, -50%)',
+                            backgroundRepeat: 'no-repeat'
 
                         }}
                     />
                 ))}
             </div>
-            {/* <img src="" alt="" /> */}
         </>
     )
 }
